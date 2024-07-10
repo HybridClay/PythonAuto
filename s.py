1597ba980133b42c716ba9b722c6ba30df8a1af9
HybridClay <165504897+HybridClay@users.noreply.github.com>
2024-07-09 20:24:48 -0400

    Add files via upload
    This python scripts allows the chrome Browser to open up the websites I will be using with each website in a new tab.
    If your Chrome Browser has profiles, go click on the Profile and using the Profile Browser, click top right with three dots> Profile> Customize your Chrome> scroll to the bottom and click the switch that says "Create desktop shortcut". Now in Desktop, right click> Properties> Ctrl+C and Paste that Target to your code to open the Browser using that Profile

---
+++ s.py
@@ -0,0 +1,29 @@
+import webbrowser
+
+# getting the path
+# chrome_path = r"C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe"
+
+# Chrome browser path to my profile:    "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 1"
+
+#This works too
+#chrome_path = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe --profile-directory=Profile 1"
+
+chrome_path = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --profile-directory='Profile 1'"
+
+# First registers the new browser
+webbrowser.register('chrome', None,
+                    webbrowser.BackgroundBrowser(chrome_path))
+
+Google = 'https://www.google.com/'
+#webbrowser.get('chrome').open(Google)
+webbrowser.open(Google)
+
+RingCentral = 'https://app.ringcentral.com/login'
+webbrowser.open_new_tab(RingCentral)
+
+textfree = 'https://messages.textfree.us/login'
+webbrowser.open_new_tab(textfree)
+
+ADP = 'https://www.adp.com/'
+webbrowser.open_new_tab(ADP)
+
